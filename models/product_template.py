d6ad5971d9eca2da8a1fcb28201d254f3f1cc6a5
sadetali <sadetali@gmail.com>
2026-03-18 16:20:30 +0400

    Add internal_variant_ids field to ProductTemplate

--- models/product_template.py
+++ models/product_template.py
@@ -1,1 +1,11 @@
+from odoo import fields, models
 
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    internal_variant_ids = fields.One2many(
+        "product.internal.variant",
+        "product_tmpl_id",
+        string="Internal Variants",
+    )
